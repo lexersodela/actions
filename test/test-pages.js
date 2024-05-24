bcbccd5dca8eb020bb0f09b9bda1ceb875af911f
Lex <lexersodela.13@gmail.com>
2024-05-24 11:34:56 +0700

    refactor: test action

---
+++ test/test-pages.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+import request from 'request';
+
+it('Hello world test', function (done) {
+    request('http://localhost:3000', function (error, response, body) {
+        expect(body).to.equal('Hello, World!');
+        done();
+    });
+});
