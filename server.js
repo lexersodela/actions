5986022fd69dbf6d9ac8eff1525c0c16b708a03a
Lex <lexersodela.13@gmail.com>
2024-05-24 11:56:53 +0700

    refactor: separate ci cd

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
     res.send('Hello, World!');
 });
 
