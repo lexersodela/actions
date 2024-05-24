d06c1c38fe2d7c3b367598ab77bb508ece07e157
Lex <lexersodela.13@gmail.com>
2024-05-24 12:45:34 +0700

    refactor: revert to hello world

--- test/test-pages.js
+++ test/test-pages.js
@@ -3,7 +3,7 @@ import request from 'request';
 
 it('Hello world test', function (done) {
     request('http://localhost:3000', function (error, response, body) {
-        expect(body).to.equal("Hello Worldz!");
+        expect(body).to.equal("Hello World!");
         done();
     });
 });
